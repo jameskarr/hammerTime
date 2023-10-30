f4ca1df0688a64f15af5df4759c43943d1a4e4b2
James Karr <jameskarrdev@gmail.com>
2023-10-30 18:59:31 -0500

    hooked appropriately

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import './App.css'
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 const HammerTime = () => {
 
-  const [selectedColor, setSelectedColor] = useState('')
-  const [cupColor, setCupColor] = useState('')
   const [headColor, setHeadColor] = useState('')
+  const [cupColor, setCupColor] = useState('')
+  const [shaftColor, setShaftColor] = useState('')
+  const [gripColor, setGripColor] = useState('')
+  const [pommelColor, setPommelColor] = useState('')
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setCupColor(selectedColor)
-    setHeadColor(selectedColor)
+  const handleHeadChange = (e) => {
+    setHeadColor(e.target.value)
   }
 
   const handleCupChange = (e) => {
-    setSelectedColor(e.target.value);
+    setCupColor(e.target.value)
   }
 
-  const handleHeadChange = (e) => {
-    setSelectedColor(e.target.value);
+  const handleShaftChange = (e) => {
+    setShaftColor(e.target.value)
+  }
+
+  const handleGripChange = (e) => {
+    setGripColor(e.target.value)
+  }
+
+  const handlePommelChange = (e) => {
+    setPommelColor(e.target.value)
   }
 
   return (
@@ -26,43 +34,72 @@ const HammerTime = () => {
 
       <h1>Color Selector</h1>
 
-      <form onSubmit={handleSubmit}>
+      <form>
+        <label htmlFor="head">Head:</label>
+          <select id="head" onChange={handleHeadChange}>
+            <option value="">Select a color</option>
+            <option value="red">Red</option>
+            <option value="blue">Blue</option>
+            <option value="green">Green</option>
+            <option value="yellow">Yellow</option>
+            <option value="orange">Orange</option>
+          </select>
 
-      <label htmlFor="cups">Head:</label>
-        <select id="cups" onChange={handleCupChange}>
-          <option value="">Select a color</option>
-          <option value="red">Red</option>
-          <option value="blue">Blue</option>
-          <option value="green">Green</option>
-          <option value="yellow">Yellow</option>
-          <option value="orange">Orange</option>
-        </select>
+        <label htmlFor="cups">Cups:</label>
+          <select id="cups" onChange={handleCupChange}>
+            <option value="">Select a color</option>
+            <option value="red">Red</option>
+            <option value="blue">Blue</option>
+            <option value="green">Green</option>
+            <option value="yellow">Yellow</option>
+            <option value="orange">Orange</option>
+          </select>
 
-      <label htmlFor="head">Cups:</label>
-        <select id="head" onChange={handleHeadChange}>
-          <option value="">Select a color</option>
-          <option value="red">Red</option>
-          <option value="blue">Blue</option>
-          <option value="green">Green</option>
-          <option value="yellow">Yellow</option>
-          <option value="orange">Orange</option>
-        </select>
+        <label htmlFor="cups">Shaft:</label>
+          <select id="cups" onChange={handleShaftChange}>
+            <option value="">Select a color</option>
+            <option value="red">Red</option>
+            <option value="blue">Blue</option>
+            <option value="green">Green</option>
+            <option value="yellow">Yellow</option>
+            <option value="orange">Orange</option>
+          </select>
 
-        <button type="submit">Submit</button>
+        <label htmlFor="cups">Grip:</label>
+          <select id="cups" onChange={handleGripChange}>
+            <option value="">Select a color</option>
+            <option value="red">Red</option>
+            <option value="blue">Blue</option>
+            <option value="green">Green</option>
+            <option value="yellow">Yellow</option>
+            <option value="orange">Orange</option>
+          </select>
+
+        <label htmlFor="cups">Pommel:</label>
+          <select id="cups" onChange={handlePommelChange}>
+            <option value="">Select a color</option>
+            <option value="red">Red</option>
+            <option value="blue">Blue</option>
+            <option value="green">Green</option>
+            <option value="yellow">Yellow</option>
+            <option value="orange">Orange</option>
+          </select>
+
+          <button type="submit">Reset</button>
       </form>
 
      
 
       <div className='theHammer'>
         <div className='upperHammer'>
-          <div className="cups"></div>
+          <div className="cups" style={{backgroundColor: cupColor}}></div>
           <div className="head" style={{backgroundColor: headColor}}></div>
           <div className="cups" style={{backgroundColor: cupColor}}></div>
         </div>
         <div className='lowerHammer'>
-          <div className="shaft"></div>
-          <div className="grip"></div>
-          <div className="pommel"></div>
+          <div className="shaft" style={{backgroundColor: shaftColor}}></div>
+          <div className="grip" style={{backgroundColor: gripColor}}></div>
+          <div className="pommel" style={{backgroundColor: pommelColor}}></div>
         </div>
       </div>
 
